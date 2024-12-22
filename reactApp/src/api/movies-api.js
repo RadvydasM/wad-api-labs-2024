35a0408d06eb8c1d0e4eeb8df92c7f69137ccf45
RadvydasM <20098619@mail.wit.ie>
2024-12-22 23:06:48 +0000

    Integration Lab - Sign up Page

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,6 +1,6 @@
 export const getMovies = async () => {
     const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=c1gfdg225ecfffffffff3612d8c1bcb4bcb&language=en-US&include_adult=false&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=022afcafedd758ad7d2c815e607e112e&language=en-US&include_adult=false&page=1`
     )
     return response.json()
   };
